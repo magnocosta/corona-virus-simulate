bfe6cb4c89d17ecb00e1cc678671a0fac21ee9fb
Magno Costa <magno.costa@m4u.com.br>
2020-03-22 18:48:28 -0300

    Refactory for better code

--- src/msat/view/MainView.kt
+++ src/msat/view/MainView.kt
@@ -1,36 +1,39 @@
-package model.msat.view
+package msat.view
 
 import msat.infra.Logger
-import msat.model.Ball
+import msat.model.Person
+import msat.model.Population
+import msat.view.converter.drawAsBall
 import java.awt.BorderLayout
 import java.awt.Color
-import java.awt.Point
-import java.awt.event.ActionEvent
-import java.awt.event.ActionListener
-import javax.swing.*
+import javax.swing.BorderFactory
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JPanel
 
 class MainView: JFrame() {
 
     /******************************************************************************************************************
-    *** View Config
-    ******************************************************************************************************************/
-    private val windowMaxWidth  = 800
+     *** View Config
+     ******************************************************************************************************************/
+    private val windowMaxWidth = 800
     private val windowMaxHeight = 800
-    private val windowTitle     = "Aprendendo sobre threads"
+    private val populationSize = 100
+    private val windowTitle = "Corona Virus Simulator"
 
     /******************************************************************************************************************
      *** View Components
      ******************************************************************************************************************/
-    private val bttStartThread  = JButton("Start Thread")
-    private val bttStopThread   = JButton("Stop Thread")
-    private val pnContainer     = JPanel(BorderLayout())
-    private val pnMain          = JPanel(null)
+    private val bttNewSimulation = JButton("New Simulation")
+    private val bttClearSimulation = JButton("Clear Simulation")
+    private val pnContainer = JPanel(BorderLayout())
+    private val pnMain = JPanel(null)
 
     /******************************************************************************************************************
      *** Others Stuffs
      ******************************************************************************************************************/
     private val logger = Logger
-    private var listOfBalls = mutableListOf<Ball>()
+    private var listOfBalls = mutableListOf<Person>()
 
     init {
         configView()
@@ -40,11 +43,11 @@ class MainView: JFrame() {
         configFrame()
         configPanels()
         configButtons()
-        population()
+        drawPopulationOnView(Population(populationSize))
     }
 
     private fun configFrame() {
-        defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+        defaultCloseOperation = DISPOSE_ON_CLOSE
         title = windowTitle
         isVisible = true
         setSize(windowMaxWidth, windowMaxHeight)
@@ -57,54 +60,31 @@ class MainView: JFrame() {
     }
 
     private fun configButtons() {
-        bttStartThread.addActionListener { countRedBalls() }
-        pnContainer.add(bttStartThread, BorderLayout.NORTH)
-        pnContainer.add(bttStopThread, BorderLayout.SOUTH)
-    }
-
-    private fun population() {
-        isVisible = false
-        for (i in 1..2) {
-            createBall()
+        bttNewSimulation.addActionListener {
+            clearPopulationOnView()
+            drawPopulationOnView(Population(300))
         }
-        isVisible = true
+        bttClearSimulation.addActionListener { clearPopulationOnView() }
+        pnContainer.add(bttNewSimulation, BorderLayout.NORTH)
+        pnContainer.add(bttClearSimulation, BorderLayout.SOUTH)
     }
 
-    private fun createBall() {
-        val ball = Ball(this.width, this.height)
-        pnMain.add(ball)
-        animate(ball, Point(this.width, this.height), 44, 1000)
-        listOfBalls.add(ball)
+    private fun drawPopulationOnView(population: Population) {
+        population.people.forEach {
+            val view = it.drawAsBall(it, this.width, this.height)
+            pnMain.add(view)
+        }
+        reDrawView()
     }
 
-    private fun countRedBalls() {
-        val doentes = listOfBalls.filter { it.colorChoose == 0 }.count()
-        val saudavel = listOfBalls.filter { it.colorChoose == 1 }.count()
-        val curado = listOfBalls.filter { it.colorChoose == 2 }.count()
-        val morto = listOfBalls.filter { it.colorChoose == 3 }.count()
-
-        JOptionPane.showMessageDialog(this, "Mortos: $morto, curados: $curado, saudavel: $saudavel, doentes: $doentes")
+    private fun clearPopulationOnView() {
+        pnMain.components.filterIsInstance<JPanel>().forEach { pnMain.remove(it) }
+        reDrawView()
     }
 
-    private fun animate(component: JComponent, newPoint: Point, frames: Int, interval: Int) {
-        val compBounds = component.bounds
-        val oldPoint = Point(compBounds.x, compBounds.y)
-        val animFrame = Point(
-            (newPoint.x - oldPoint.x) / frames,
-            (newPoint.y - oldPoint.y) / frames
-        )
-        Timer(interval, object : ActionListener {
-            var currentFrame = 0
-            override fun actionPerformed(e: ActionEvent) {
-                component.setBounds(
-                    oldPoint.x + animFrame.x * currentFrame,
-                    oldPoint.y + animFrame.y * currentFrame,
-                    compBounds.width,
-                    compBounds.height
-                )
-                if (currentFrame != frames) currentFrame++ else (e.getSource() as Timer).stop()
-            }
-        }).start()
+    private fun reDrawView() {
+        pnMain.revalidate()
+        pnMain.repaint()
     }
 
 }
