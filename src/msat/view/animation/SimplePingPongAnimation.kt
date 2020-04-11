5124907612aca334be9147528486356bb8efaa7c
Magno Costa <magno.costa@m4u.com.br>
2020-04-11 13:01:12 -0300

    Add collision detection

--- src/msat/view/animation/SimplePingPongAnimation.kt
+++ src/msat/view/animation/SimplePingPongAnimation.kt
@@ -1,12 +1,15 @@
 package msat.view.animation
 
+import msat.infra.Logger
 import java.awt.Component
+import java.awt.Rectangle
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
+import javax.swing.JPanel
 import javax.swing.Timer
 import kotlin.random.Random
 
-class SimplePingPongAnimation(private val component: Component, private val container: Component) : ActionListener {
+class SimplePingPongAnimation(private val target: Component, private val container: JPanel) : ActionListener {
 
     companion object {
         const val FRAME_RATE = 1
@@ -15,7 +18,10 @@ class SimplePingPongAnimation(private val component: Component, private val cont
     enum class DirectionX { LEFT, RIGHT }
     enum class DirectionY { UP, DOWN }
 
+    private val logger = Logger
     private val timer = Timer(5, this)
+    private val objectsOnContainer = container.components.filterIsInstance<JPanel>().filter { it != target }
+
     private var directionX = if (Random.nextBoolean()) DirectionX.LEFT else DirectionX.RIGHT
     private var directionY = if (Random.nextBoolean()) DirectionY.UP else DirectionY.DOWN
 
@@ -25,35 +31,52 @@ class SimplePingPongAnimation(private val component: Component, private val cont
 
     override fun actionPerformed(event: ActionEvent?) {
 
+        if (!target.isVisible) {
+            logger.debug("Stopping animation, because object is not visible $target")
+            timer.stop()
+            return
+        }
+
         val newX = newPositionX()
         val newY = newPositionY()
 
-        component.setLocation(newX, newY)
-        component.repaint()
+        if (hasCollisionWithOtherObject(newX, newY)) {
+            logger.debug("Collision detected on $target")
+        }
+
+        target.setLocation(newX, newY)
+        target.repaint()
+
+//        logger.debug("Object moved to new position $target")
     }
 
     private fun newPositionX(): Int {
-        if (component.x >= container.width) {
+        if (target.x >= container.width) {
             directionX = DirectionX.LEFT
         }
 
-        if (component.x <= 0) {
+        if (target.x <= 0) {
             directionX = DirectionX.RIGHT
         }
 
-        return if (directionX == DirectionX.RIGHT) component.x + FRAME_RATE else component.x - FRAME_RATE
+        return if (directionX == DirectionX.RIGHT) target.x + FRAME_RATE else target.x - FRAME_RATE
     }
 
     private fun newPositionY(): Int {
-        if (component.y >= container.height) {
+        if (target.y >= container.height) {
             directionY = DirectionY.UP
         }
 
-        if (component.y <= 0) {
+        if (target.y <= 0) {
             directionY = DirectionY.DOWN
         }
 
-        return if (directionY == DirectionY.DOWN) component.y + FRAME_RATE else component.y - FRAME_RATE
+        return if (directionY == DirectionY.DOWN) target.y + FRAME_RATE else target.y - FRAME_RATE
+    }
+
+    private fun hasCollisionWithOtherObject(newX: Int, newY: Int): Boolean {
+        val rectangle = Rectangle(newX, newY, target.width, target.height)
+        return objectsOnContainer.any { rectangle.intersects(it.bounds) }
     }
 
 }
